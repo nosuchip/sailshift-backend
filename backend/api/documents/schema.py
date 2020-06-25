0318f65496e0e9493300c2d644fefa14d748e24f
Alex Xela <nosuchip@gmail.com>
2020-06-25 23:17:45 +0700

    Allow empty fields to be null

--- backend/api/documents/schema.py
+++ backend/api/documents/schema.py
@@ -2,36 +2,36 @@ from marshmallow import Schema, fields
 
 
 class DocumentCreateSchema(Schema):
-    price = fields.Number(required=False)
+    price = fields.Number(required=False, allow_none=True)
     title = fields.Str(required=True)
-    organization = fields.Str(required=False)
-    department = fields.Str(required=False)
-    description = fields.Str(required=False)
+    organization = fields.Str(required=False, allow_none=True)
+    department = fields.Str(required=False, allow_none=True)
+    description = fields.Str(required=False, allow_none=True)
 
 
 class DocumentUpdateSchema(Schema):
     id = fields.Str(required=True)
     price = fields.Number(required=True)
-    url = fields.Str(required=False)
-    title = fields.Str(required=False)
-    organization = fields.Str(required=False)
-    department = fields.Str(required=False)
-    description = fields.Str(required=False)
-    text = fields.Str(required=False)
+    url = fields.Str(required=False, allow_none=True)
+    title = fields.Str(required=False, allow_none=True)
+    organization = fields.Str(required=False, allow_none=True)
+    department = fields.Str(required=False, allow_none=True)
+    description = fields.Str(required=False, allow_none=True)
+    text = fields.Str(required=False, allow_none=True)
 
 
 class DocumentGrantSchema(Schema):
     document_id = fields.Str(required=True)
     user_id = fields.Str(required=True)
     expires_in = fields.Int(required=True)
-    notify_user = fields.Boolean(required=False)
+    notify_user = fields.Boolean(required=False, allow_none=True)
 
 
 class DocumentSearchSchema(Schema):
     query = fields.Str(required=True)
-    title = fields.Str(required=False)
-    organization = fields.Str(required=False)
-    department = fields.Str(required=False)
-    text = fields.Str(required=False)
-    page = fields.Int(required=False, default=0)
-    page_size = fields.Int(required=False, default=10)
+    title = fields.Str(required=False, allow_none=True)
+    organization = fields.Str(required=False, allow_none=True)
+    department = fields.Str(required=False, allow_none=True)
+    text = fields.Str(required=False, allow_none=True)
+    page = fields.Int(required=False, allow_none=True, default=0)
+    page_size = fields.Int(required=False, allow_none=True, default=10)
